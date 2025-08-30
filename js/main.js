673e4a7c55b75d22d30f400e83ffe22ee71b4396
root <root@localhost.localdomain>
2025-08-30 09:59:41 -0500

    Estructura inicial del sitio web3

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 // Archivo inicial de JavaScript
-function randomColor() {
-    return `rgb(${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)})`;
+// Colores hexadecimales sugeridos
+const colors = ['#008000', '#497D15', '#9AE630', '#178236'];
+
+function getRandomHexColor() {
+    return colors[Math.floor(Math.random() * colors.length)];
 }
+
 document.querySelectorAll('header, nav, main, section, aside, footer').forEach(el => {
-    el.style.background = randomColor();
+    el.style.background = getRandomHexColor();
 });
